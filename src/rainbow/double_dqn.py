2423e68fb78847bfbfdc7346d885929dc336286d
Matheus Teotonio <matheusvncs160@gmail.com>
2025-11-16 17:45:04 -0300

    Adds double_dqn implementation

---
+++ src/rainbow/double_dqn.py
@@ -0,0 +1,78 @@
+import numpy as np
+import torch
+import torch.nn.functional as F
+from src.doom.game import Game
+
+from dqn import DQNAgent
+
+class DoubleDQNAgent(DQNAgent):
+    """Double DQN Agent interacting with environment.
+    
+    Inherits from DQNAgent and overrides methods to implement Double DQN.
+    """
+
+    def __init__(
+        self,
+        game: Game,
+        memory_size: int,
+        batch_size: int,
+        target_update: int,
+        epsilon_decay: float,
+        seed: int,
+        max_epsilon: float = 1.0,
+        min_epsilon: float = 0.1,
+        gamma: float = 0.99,
+    ):
+        """Initialization.
+        
+        Args:
+            game (Game): Doom game environment
+            memory_size (int): length of memory
+            batch_size (int): batch size for sampling
+            target_update (int): period for target model's hard update
+            epsilon_decay (float): step size to decrease epsilon
+            lr (float): learning rate
+            max_epsilon (float): max value of epsilon
+            min_epsilon (float): min value of epsilon
+            gamma (float): discount factor
+        """
+        super().__init__(
+            game,
+            memory_size,
+            batch_size,
+            target_update,
+            epsilon_decay,
+            seed,
+            max_epsilon,
+            min_epsilon,
+            gamma,
+        )
+
+    """
+    Override _compute_dqn_loss to implement Double DQN logic.
+    The DQNAgent utilizes a single network for both action selection and evaluation.
+    In Double DQN, the action selection is done using the online network,
+    while the evaluation is done using the target network.
+    """
+    def _compute_dqn_loss(self, samples: dict[str, np.ndarray]) -> torch.Tensor:
+        """Return dqn loss."""
+        device = self.device  # for shortening the following lines
+        state = torch.FloatTensor(samples["obs"]).to(device)
+        next_state = torch.FloatTensor(samples["next_obs"]).to(device)
+        action = torch.LongTensor(samples["acts"].reshape(-1, 1)).to(device)
+        reward = torch.FloatTensor(samples["rews"].reshape(-1, 1)).to(device)
+        done = torch.FloatTensor(samples["done"].reshape(-1, 1)).to(device)
+        
+        # G_t   = r + gamma * v(s_{t+1})  if state != Terminal
+        #       = r                       otherwise
+        curr_q_value = self.dqn(state).gather(1, action)
+        next_q_value = self.dqn_target(next_state).gather(  # Double DQN
+            1, self.dqn(next_state).argmax(dim=1, keepdim=True)
+        ).detach()
+        mask = 1 - done
+        target = (reward + self.gamma * next_q_value * mask).to(self.device)
+
+        # calculate dqn loss
+        loss = F.smooth_l1_loss(curr_q_value, target)
+
+        return loss
